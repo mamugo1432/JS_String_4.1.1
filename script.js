fea064e8bf9e19a507b2bf9dcc473719252423a3
mamugo1432 <mamugo1432@gmail.com>
2025-04-01 08:25:07 +0200

    Subiendo los enunciados

---
+++ script.js
@@ -0,0 +1,21 @@
+/*Declara una variable cuyo nombre sea cadena y
+ tenga el valor “Hola Mundo”.*/
+/*Muestra por consola la longitud de la cadena.*/
+/*Muestra por consola la cadena con todos sus caracteres en
+ mayúsculas.*/
+/*Muestra por consola la cadena con todos sus caracteres
+ en minúsculas.*/
+/*Muestra por consola la posición de la cadena en
+ la que se encuentra la letra o.*/
+/*Muestra por consola la posición de la cadena en la
+ que se encuentra la cadena “Hola”.*/
+/*Reemplaza la cadena “Mundo” por la cadena “Youtube”
+ y muestra el resultado.*/
+/*Extrae la segunda parte de la cadena y muéstrala por consola.*/
+/*Comprueba si la cadena empieza por “h”.*/
+/*Comprueba si la cadena empieza por “H”.*/
+/*Muestra Utilizando por TEMPLATE consola la STRINGS
+ letra JAVASCRIPT*/
+/* “r” declara 10 veces.*/
+ /*3 variables con tu nombre, apellidos y edad y
+  muéstralas por consola introduciéndolas dentro de una cadena.*/
